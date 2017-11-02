e9db8d9c0832bb58c4c9001ef1be042c2e570880
Julian Hinsch <julianhinsch@vpm-g8xsfb2.villagegroup.local>
2017-11-02 09:14:33 -0400

    added cors to routes

--- routes/api.php
+++ routes/api.php
@@ -17,34 +17,30 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/posts', 'PostsController@index');
+Route::get('/posts', array('middleware' => 'cors', 'uses' => 'PostsController@index'));
 
-Route::get('/views', 'ViewsController@index');
+Route::get('/views', array('middleware' => 'cors', 'uses' => 'ViewsController@index'));
 
+Route::get('/action-types', array('middleware' => 'cors', 'uses' => 'ActionTypesController@index'));
 
+Route::get('/actions', array('middleware' => 'cors', 'uses' => 'ActionsController@index'));
 
-Route::get('/action-types', 'ActionTypesController@index');
+Route::get('/firms', array('middleware' => 'cors', 'uses' => 'FirmsController@index'));
 
-Route::get('/actions', 'ActionsController@index');
+Route::get('/ip-cities', array('middleware' => 'cors', 'uses' => 'IpCitiesController@index'));
 
-Route::get('/firms', 'FirmsController@index');
+Route::get('/products', array('middleware' => 'cors', 'uses' => 'ProductsController@index'));
 
-Route::get('/ip-cities', 'IpCitiesController@index');
+Route::get('/ip-countries', array('middleware' => 'cors', 'uses' => 'IpCountriesController@index'));
 
-Route::get('/products', 'ProductsController@index');
-// under this!!!
-Route::get('/ip-countries', 'IpCountriesController@index');
+Route::get('/ip-states', array('middleware' => 'cors', 'uses' => 'IpStatesController@index'));
 
-Route::get('/ip-states', 'IpStatesController@index');
+Route::get('/ip-addresses', array('middleware' => 'cors', 'uses' => 'IpAddressesController@index'));
 
-Route::get('/ip-addresses', 'IpAddressesController@index');
+Route::get('/migrations', array('middleware' => 'cors', 'uses' => 'MigrationsController@index'));
 
-Route::get('/migrations', 'MigrationsController@index');
+Route::get('/user-actions', array('middleware' => 'cors', 'uses' => 'UserActionsController@index'));
 
-Route::get('/user-actions', 'UserActionsController@index');
-
-Route::get('/users', 'UsersController@index');
-
-
+Route::get('/users', array('middleware' => 'cors', 'uses' => 'UsersController@index'));
 
 Route::get('/example', array('middleware' => 'cors', 'uses' => 'ExampleController@index'));
